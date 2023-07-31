cf8c060a21cc2afa15c0b413f18456b8f2afe600
FilipStojkovich <119892777+FilipStojkovich@users.noreply.github.com>
2023-07-31 23:16:13 +0200

    Add files via upload

---
+++ BurgerApp.App/Controllers/OrderController.cs
@@ -0,0 +1,64 @@
+﻿using BurgerApp.Services.Implementations;
+using BurgerApp.Services.Interfaces;
+using BurgerApp.ViewModels.BurgerViewModel;
+using BurgerApp.ViewModels.OrderViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BurgerApp.App.Controllers
+{
+    public class OrderController : Controller
+    {
+        private IOrderService _orderService;
+        public OrderController(IOrderService _orderService)
+        {
+            this._orderService = _orderService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _orderService.GetOrdersForCards());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            return View(await _orderService.GetOrderDetails(id));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            return View(await _orderService.DeleteOrderById(id));
+        }
+
+        public IActionResult Create()
+        {
+            OrderViewModel orderViewModel = new OrderViewModel();
+
+            return View(orderViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(OrderViewModel orderViewModel)
+        {
+            await _orderService.CreateOrder(orderViewModel);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            OrderViewModel orderViewModel = await _orderService.GetOrderForEditing(id);
+
+            return View(orderViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(OrderViewModel orderViewModel)
+        {
+            await _orderService.EditOrder(orderViewModel);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BurgerApp.App/Controllers/UserController.cs
@@ -0,0 +1,64 @@
+﻿using BurgerApp.Services.Interfaces;
+using BurgerApp.ViewModels.BurgerViewModel;
+using BurgerApp.ViewModels.UserViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BurgerApp.App.Controllers
+{
+    public class UserController : Controller
+    {
+        private IUserService _userService;
+
+        public UserController(IUserService _userService)
+        {
+            this._userService = _userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _userService.GetUsersForCards());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            return View(await _userService.GetUserDetails(id));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            return View(await _userService.DeleteUserById(id));
+        }
+
+        public IActionResult Create()
+        {
+            UserViewModel userViewModel = new UserViewModel();
+
+            return View(userViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(UserViewModel userViewModel)
+        {
+            await _userService.CreateUser(userViewModel);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            UserViewModel userViewModel = await _userService.GetUserForEditing(id);
+
+            return View(userViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(UserViewModel userViewModel)
+        {
+            await _userService.EditUser(userViewModel);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
